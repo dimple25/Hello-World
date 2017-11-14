a64b473c909007ac1fedda983b2c78a118fe3b7e
dimple25 <33650900+dimple25@users.noreply.github.com>
2017-11-14 13:25:35 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,18 @@
 <?php echo "Hello World ";?>
+<html>
+  <table style="border:1px solid #333;">
+    <th colspan="2"> Code of Conduct </th>
+    <tr>
+      <td> 1. </td>
+      <td> Write your first blog post </td>
+    </tr>
+    <tr>
+      <td> 2. </td>
+      <td> Add an About page </td>
+    </tr>
+    <tr>
+      <td> 3. </td>
+      <td> View your site </td>
+    </tr>
+  </table>
+</html>
